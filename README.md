6bb7cd10c16adb3daa4f1e71321bd8286c153e4d
Ian Harvey <email@ianharvey.ca>
2020-07-31 17:41:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# react-boot-strap-template
+
+A basic react-bootstrap template.
+
+## Installation
+
++ git clone https://github.com/ianmharvey/react-template.git
++ chdir into this project's root directory `chdir react-template`
++ Run `npm install react-bootstrap bootstrap` to install the app's dependencies
+
+## Usage
+
++ In console window type `chdir react-template` 
++ Run `npm start`
++ Access the running app in a browser at `localhost:xxxx`
++ Run `yarn build` to create production version
+
+## .htacess
+
+The .htaccess file in the `/public` directory prevents users from getting a 404 error when attempting to load a url path directly, or page reload. This is not a problem when running localhost, but the issue occurs on the external web server running a production build.
+
+## Demo
+
+View demo at http://template.ianharvey.ca/
---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require("express"),
+      app = express(),
+      debug = require("debug")("express-urlrewrite"),
+      toRegexp = require("path-to-regexp"),
+      URL = require("url"),
+      hostname = "localhost",
+      port = 3000;
+
+module.exports = rewrite;
+
+function rewrite(src, dst) {
+  var keys = [], re, map;
+
+  if (dst) {
+    re = toRegexp(src, keys);
+    map = toMap(keys);
+    debug("rewrite %s -> %s    %s", src, dst, re);
+  } else {
+    debug("rewrite current route -> %s", src);
+  }
+
+  return function(req, res, next) {
+    var orig = req.url;
+    var m;
+    if (dst) {
+      m = re.exec(orig);
+      if (!m) {
+        return next();
+      }
+    }
+    req.url = req.originalUrl = (dst || src).replace(/\$(\d+)|(?::(\w+))/g, function(_, n, name) {
+      if (name) {
+        if (m) return m[map[name].index + 1];
+        else return req.params[name];
+      } else if (m) {
+        return m[n];
+      } else {
+        return req.params[n];
+      }
+    });
+    debug("rewrite %s -> %s", orig, req.url);
+    if (req.url.indexOf("?") > 0) {
+      req.query = URL.parse(req.url, true).query;
+      debug("rewrite updated new query", req.query);
+    }
+    if (dst) next();
+    else next("route");
+  }
+}
+
+function toMap(params) {
+  var map = {};
+
+  params.forEach(function(param, i) {
+    param.index = i;
+    map[param.name] = param;
+  });
+
+  return map;
+}
+
+app.use(rewrite("/template", "/template.html"))
+app.use(rewrite("/grid", "/grid.html"))
+app.use(rewrite("/holygrail", "/holygrail.html"))
+app.use(rewrite("/holygrail-template", "/holygrail-template.html"))
+app.use(rewrite("/holygrail-template-with-cards", "/holygrail-template-with-cards.html"))
+
+app.use(express.static("public"))
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
